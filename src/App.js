215b77e85e2370ad7de9f0176d18dd3b8dc2b321
Ahad <sa07753@st.habib.edu.pk>
2024-04-21 15:54:06 +0500

    admin page UI commit1

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from "react-route
 import Search from './pages/Search';
 import Navbar from './components/Navbar';
 import OrderHistory from './pages/OrderHistory';
+import AdminOrder from '../src/AdminPages/adminOrder';
 import Footer from './components/Footer';
 import ContactUs from './pages/ContactUs';
 import LogIn from './pages/LogIn';
+import AdminProducts from './AdminPages/adminProducts';
 
 function Main() {
   const location = useLocation();
 
   return (
     <div className="App page-wrapper">
-      {location.pathname !== '/' && <Navbar />}
+      {(location.pathname !== '/' && location.pathname !== '/adminorders' && location.pathname !== '/adminProducts'
+    ) && <Navbar />}
       <div className="main-content">
         <Routes>
           <Route exact path="/" element={<LogIn />} />
           <Route path="/search" element={<Search />} />
           <Route path="/history" element={<OrderHistory />} />
           <Route path="/faqs" element={<ContactUs />} />
+          <Route path="/adminorders" element={<AdminOrder />} />
+          <Route path="/adminProducts" element={<AdminProducts />} />
         </Routes>
       </div>
-      <Footer />
+      {(location.pathname !== '/') && <Footer />}
     </div>
   );
 }
+
 
 function App() {
   return (
