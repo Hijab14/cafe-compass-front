01f5d9f101846c455a2e647b766fd93d576d9bec
Hijab14 <hf07068@st.habib.edu.pk>
2024-04-21 16:03:52 +0500

    BEfore merge

--- src/pages/checkOut/index.jsx
+++ src/pages/checkOut/index.jsx
@@ -6,37 +6,6 @@ import { useSelector } from "react-redux";
 function Index() {
   const cartItems = useSelector((state) => state.items);
 
-//   const putorder = async (address, phone, total = 0) => {
-//     try {
-//       // API Call
-//       const response = await fetch(`${host}/api/orders/addorder`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           address,
-//           phone,
-//           total,
-//           cartItems,
-//           email,
-//           location,
-//         }),
-//       });
-
-//       if (response.ok) {
-//         // If the response is successful (status code 2xx), show the alert
-//         alert("Order placed successfully");
-//       } else {
-//         // Handle errors if the response is not successful
-//         console.error("Error sending order details:", response.statusText);
-//       }
-//     } catch (error) {
-//       // Handle network errors or other exceptions
-//       console.error("Error:", error.message);
-//     }
-//   };
-
   return (
     <>
     <div>
@@ -53,7 +22,7 @@ function Index() {
           type="submit"
           href="/"
           style={{backgroundColor:'#0f9ea7', color:'white'}}
-        //   onClick={() => putorder(address, phone)}
+          onClick={() => putorder(email,cartItems)}
         >
           Confirm Order
         </button>
